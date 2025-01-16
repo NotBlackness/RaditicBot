0f9119ef0e98bff1b4ef0333e7197523f8d57ae8
NotBlackness <notblackness@gmail.com>
2025-01-16 06:51:01 +0000

    .

---
+++ src/commands/🌾 Economy/top-cash.js
@@ -0,0 +1,50 @@
+const User = require('../../Schemas/userAccount.js');
+const { EmbedBuilder } = require('discord.js');
+const { emoji, color } = require('../../config');
+
+module.exports = {
+  usage: 'top-cash <limit>',
+  name: 'top-cash',
+  aliases: ['topcash', 'top-c', 'topc'],
+  description: 'Display the top users based on their balance.',
+  async execute({ msg, args }) {
+    try {
+      let limit = 5;
+      if (args[0]) {
+        limit = parseInt(args[0]);
+        if (isNaN(limit) || limit <= 0 || limit > 25) {
+          return msg.reply('Please provide a valid limit of users (1-25) to display.');
+        }
+      }
+      const topUsers = await User.find().sort({ balance: -1 }).limit(limit);
+
+      if (topUsers.length === 0) {
+        return msg.reply('No users found.');
+      }
+
+      // Find the author's rank
+      const authorData = await User.findOne({ userId: msg.author.id });
+      const authorRank = authorData ? await User.countDocuments({ balance: { $gt: authorData.balance } }) + 1 : 'N/A';
+
+      const leaderboard = new EmbedBuilder()
+        .setTitle(`Top ${limit} Global Users by Balance`)
+        .setColor(color.default)
+        .setDescription(`Your rank: #${authorRank}`)
+        .setTimestamp();
+
+      for (let i = 0; i < topUsers.length; i++) {
+        const user = await msg.client.users.fetch(topUsers[i].userId);
+        if (!user) continue;
+
+        leaderboard.addFields(
+          { name: `#${i + 1}. ${user.username}`, value: `Balance: **__${topUsers[i].balance.toLocaleString()}__** ${emoji.radigem} RG coins`, inline: true },
+        )
+      }
+
+      msg.reply({ embeds: [leaderboard] });
+    } catch (error) {
+      console.error('An error occurred while fetching top users:', error);
+      msg.reply('An error occurred while fetching top users.');
+    }
+  },
+};
