dbc6193538ebdde3a407912c3837ba785588a73e
NotBlackness <notblackness@gmail.com>
2025-01-23 18:56:15 +0000

    .

--- src/index.js
+++ src/index.js
@@ -105,8 +105,8 @@ const { Kazagumo, Plugins } = require('kazagumo');
 
 const Nodes = [{
   name: 'RaditicMusic',
-  url: 'lava-v3.ajieblogs.eu.org:80',
-  auth: 'https://dsc.gg/ajidevserver',
+  url: 'lavalink.jirayu.net:13592',
+  auth: 'youshallnotpass',
   secure: false
 }];
 
