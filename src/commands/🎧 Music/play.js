e2a7b747cb3babb7dc5055c29a497437f3b9c728
NotBlackness <notblackness@gmail.com>
2025-03-05 04:58:17 +0000

    .

--- src/commands/🎧 Music/play.js
+++ src/commands/🎧 Music/play.js
@@ -2,10 +2,10 @@ const Volume = require('../../Schemas/volumeSchema');
 const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  usage: 'play <song name>',
+  usage: 'play <song name or URL>',
   name: 'play',
   aliases: ['p'],
-  description: 'Plays a song.',
+  description: 'Plays a song or playlist from a name or URL.',
   async execute({ msg, args, client }) {
     const query = args.slice(1).join(" ");
     const { channel } = msg.member.voice;
@@ -15,31 +15,34 @@ module.exports = {
       return msg.reply("I don't have permission to join or speak in your voice channel!");
     }
 
-    // Get the custom volume for this guild or set it to the default volume (40)
-    const savedVolume = await Volume.findOne({ guildId: msg.guild.id });
-    const volume = savedVolume ? savedVolume.volume : 50; // Default to 50 if no custom volume
+    if (!query) return msg.reply("Please provide a song name or a valid URL!");
 
+    // Get the custom volume for this guild or set it to the default volume (50)
+    const savedVolume = await Volume.findOne({ guildId: msg.guild.id });
+    const volume = savedVolume ? savedVolume.volume : 50;
 
     // Create the player with the volume set
     let player = await client.manager.createPlayer({
       guildId: msg.guild.id,
       textId: msg.channel.id,
       voiceId: channel.id,
-      volume: volume,  // Apply saved or default volume
+      volume: volume,
     });
 
+    // Search for a song or URL
     let result = await client.manager.search(query, { requester: msg.author });
+
     if (!result.tracks.length) return msg.reply("No results found!");
 
-    if (result.type === "PLAYLIST") player.queue.add(result.tracks);
-    else player.queue.add(result.tracks[0]);
+    // Handle playlists and single tracks
+    if (result.type === "PLAYLIST") {
+      player.queue.add(result.tracks);
+      msg.reply(`Queued **${result.tracks.length}** songs from **${result.playlistName}**.`);
+    } else {
+      player.queue.add(result.tracks[0]);
+      msg.reply(`Queued **${result.tracks[0].title}**.`);
+    }
 
     if (!player.playing && !player.paused) player.play();
-
-    return msg.reply({
-      content: result.type === "PLAYLIST" 
-        ? `Queued ${result.tracks.length} from ${result.playlistName}` 
-        : `Queued ${result.tracks[0].title}`,
-    });
   }
 };
